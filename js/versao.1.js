8b2305872f41c45a7822b537c4a8fe8630330cf9
nogueiraleonardo <leojunio007@gmail.com>
2020-08-13 19:04:26 -0300

    Atualização 3.3

--- js/versao.1.js
+++ js/versao.1.js
@@ -4,3 +4,17 @@ window.addEventListener('scroll', function(){
     let header = document.querySelector('header');
     header.classList.toggle('scrolling-active', window.scrollY > 0);
 })
+
+//Campo de Busca 
+let busca = document.querySelector('#txt-busca').value;
+
+
+
+/*
+function validaBusca() {
+    
+}
+
+
+https://api.twitter.com/1.1/search/tweets.json */
+
