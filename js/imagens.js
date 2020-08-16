d0dcd35ffba71df647da17332a5545d9e1063ad4
eMatheus23 <eliasmatheus@hotmail.com>
2020-08-16 13:56:59 -0300

    Modificação nos botẽs das abas
    Troquei o nome das classes dos botões  das abas, tanto
    no HTML quando no CSS para facilitar o uso do JavaScript.
    Agora, quando um botão é selecionado, o JavaScript adiciona uma classe
    chamada botaoAbasSelecionado, que modifica a aparencia do botão.
    Quando um segundo botão é clicado, o JS remove a classe de seleção do
    primeiro e adiciona a classe ao segundo.
    O botão dos Tweets vem selecionado por padrão no HTML e a exibição das
    imagens fica oculta por padrão no Media Query do CSS.
    
    Adicionei também um link, dentro da janela modal para o usuário visitar
    o Tweeet original da foto.

--- js/imagens.js
+++ js/imagens.js
@@ -1,30 +1,50 @@
 /* --------------------------- Esconder Divs no Mobile --------------------------- */
+// Declara variáveis com os endereços dos elementos
+const containerTweets = document.getElementById('tweets'), 
+containerImagens = document.getElementById('imagens'),
+botaoTweets = document.getElementById('tweetMobile'),
+botaoImagens = document.getElementById('imagensMobile');
 
-/* ------ Esconder ambos quando sair do media query do mobile -------------------- */
+
+/* ------ Esconder abas quando sair do media query do mobile -------------------- */
 // fonte: https://www.w3schools.com/howto/howto_js_media_queries.asp
-
 var mql = window.matchMedia("(max-width: 700px)") // Define a largura onde as mudanças ocorrerão
 
 function esconderAmbos(x) {
-    if (x.matches) { 
+    if (x.matches) {         
         /* Se a tela tiver 700px de largura ou menos */
-        document.getElementById('tweets').style.display = 'block';
-        document.getElementById('imagens').style.display = 'none';
+        if (botaoImagens.classList == 'botaoAbas botaoAbasSelecionado') { 
+            /* Verifica se o usuário selecionou o botão imagens previamente */
+            containerTweets.style.display = 'none';
+            containerImagens.style.display = 'flex';
+        } else {
+            /* Se não selecionou */
+            containerTweets.style.display = 'block';
+            containerImagens.style.display = 'none';
+        }
+
     } else {
-        /* Se a tela tiver mais de 700px de largura */
-        document.getElementById('imagens').style.display = 'flex';
-        document.getElementById('tweets').style.display = 'block';
-        document.getElementById('tweets').focus();
+        /* Se a tela tiver mais de 700px de largura, volta para as configurações originais */
+        containerImagens.style.display = 'flex';
+        containerTweets.style.display = 'block';
     } 
 }
 
 mql.addListener(esconderAmbos) // Cria um função que fica "escutando" mudanças de estado
 
 
-
+/* -------------- Esconde os tweets quando o botao das imagens é clicado ---------- */
 function esconderTweets() {
-    document.getElementById('imagens').style.display = 'flex';
-    document.getElementById('tweets').style.display = 'none';
+    /* Oculta o container dos tweets e revela o das imagens */
+    containerImagens.style.display = 'flex';
+    containerTweets.style.display = 'none';
+
+    /*  Quando um botão de aba é selecionado, essa função adiciona uma classe chamada "botaoAbasSelecionado" 
+        a este botão e remove a mesma classe do outro botão. */
+    botaoTweets.classList.remove("botaoAbasSelecionado"); // O botão Tweets vem selecionado por padrão
+    botaoImagens.classList.add("botaoAbasSelecionado"); 
+    
+    
     /*
     if (document.getElementById('Imagens') !== undefined) {
         if (document.getElementById('Imagens').style.display == 'flex') {
@@ -38,10 +58,15 @@ function esconderTweets() {
     */
 }
 
-
+/* -------------- Esconde as imagens quando o botao dos tweets é clicado ---------- */
 function esconderImagens() {
-    document.getElementById('tweets').style.display = 'block';
-    document.getElementById('imagens').style.display = 'none';
+    /* Oculta o container das imagens e revela o dos Tweets */
+    containerTweets.style.display = 'block';
+    containerImagens.style.display = 'none';
+
+    /* Faz a troca das classes dos botões */
+    botaoImagens.classList.remove("botaoAbasSelecionado"); 
+    botaoTweets.classList.add("botaoAbasSelecionado"); 
     /*
     if (document.getElementById('Tweets') !== undefined) {
         if (document.getElementById('Tweets').style.display == 'block') {
