109f5a4cfd10039c63f2c28b216d83e4ec3e4826
eMatheus23 <eliasmatheus@hotmail.com>
2020-08-16 11:40:25 -0300

    Resolução do problema do clique duplo e media
    O problema do duplo clique (Quebrando a página quando uma aba era
    clicada novamente), foi resolvido ao tirar a lógica das funções
    esconderTweets() e esconderImagens().
    A lógica antiga ainda está em comentário no documento, para
    entendimento da mudança.
    Deve ser removido nas próximas versões.
    
    Criada uma função que monitora a largura da tela para retornar os
    elementos ao estado normal em telas maiores.
    
    Mudança no nome das ID, pois estavam começando com letras maíusculas.

--- js/imagens.js
+++ js/imagens.js
@@ -1,9 +1,31 @@
 /* --------------------------- Esconder Divs no Mobile --------------------------- */
 
+/* ------ Esconder ambos quando sair do media query do mobile -------------------- */
+// fonte: https://www.w3schools.com/howto/howto_js_media_queries.asp
+
+var mql = window.matchMedia("(max-width: 700px)") // Define a largura onde as mudanças ocorrerão
+
+function esconderAmbos(x) {
+    if (x.matches) { 
+        /* Se a tela tiver 700px de largura ou menos */
+        document.getElementById('tweets').style.display = 'block';
+        document.getElementById('imagens').style.display = 'none';
+    } else {
+        /* Se a tela tiver mais de 700px de largura */
+        document.getElementById('imagens').style.display = 'flex';
+        document.getElementById('tweets').style.display = 'block';
+        document.getElementById('tweets').focus();
+    } 
+}
+
+mql.addListener(esconderAmbos) // Cria um função que fica "escutando" mudanças de estado
 
 
 
-function EsconderTweets() {
+function esconderTweets() {
+    document.getElementById('imagens').style.display = 'flex';
+    document.getElementById('tweets').style.display = 'none';
+    /*
     if (document.getElementById('Imagens') !== undefined) {
         if (document.getElementById('Imagens').style.display == 'flex') {
             document.getElementById('Imagens').style.display = 'none';
@@ -13,10 +35,14 @@ function EsconderTweets() {
             document.getElementById('Tweets').style.display = 'none';
         }
     }
+    */
 }
 
 
-function EsconderImagens() {
+function esconderImagens() {
+    document.getElementById('tweets').style.display = 'block';
+    document.getElementById('imagens').style.display = 'none';
+    /*
     if (document.getElementById('Tweets') !== undefined) {
         if (document.getElementById('Tweets').style.display == 'block') {
             document.getElementById('Tweets').style.display = 'none';
@@ -26,7 +52,10 @@ function EsconderImagens() {
             document.getElementById('Imagens').style.display = 'none';
         }
     }
+    */
 }
+
+
 /* ---------------------------  Janela Modal Imagens ----------------------- */
 function cliqueModal(img) {
     const modalJanela = document.getElementById('janela-modal');
