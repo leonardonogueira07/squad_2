bf490ec82c57a449f98242f8e1773aaa6bbf442f
Miguel Malagoli <miguel.g.malagoli@gmail.com>
2020-08-24 22:03:08 -0300

    Revisão final

--- js/login.js
+++ js/login.js
@@ -6,29 +6,20 @@ function validarForm() {
     var senha = document.getElementById("senha");
     var erroUsuario = document.getElementById("erroUsuario");
     var erroSenha = document.getElementById("erroSenha");
-       if(nome.value =="academy@cocreare.com.br" && senha.value =="123456"){
+
+    if(nome.value =="academy@cocreare.com.br" && senha.value =="123456"){
         window.location.href = "buscas.html";
         console.log("FUNCIONA");
         return true;
+    }
+    else{
+        if(nome.value!="academy@cocreare.com.br" || nome.value ==''){
+            erroUsuario.innerHTML = "Usuario Incorreto!";
         }
-        else{
-            if(nome.value!="academy@cocreare.com.br" || nome.value ==''){
-               erroUsuario.innerHTML = "Usuario Incorreto!";
-            }
-            if(senha.value!="123456" || senha.value ==''){
-                erroSenha.innerHTML = "Senha Incorreta!";
-            }
-            return false;
+        if(senha.value!="123456" || senha.value ==''){
+            erroSenha.innerHTML = "Senha Incorreta!";
         }
-        
-        
-        
-       
-   
-    
-   
-    
-
-
+        return false;
+    }
 
 }
